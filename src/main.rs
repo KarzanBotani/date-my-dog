ce24c34dfbe10c6eda2beed117dd16d04b04e523
Karzan Botani <karzan@botani.nu>
2021-09-13 19:16:27 +0200

    Just a small hello-world file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, http::StatusCode, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+  HttpResponse::Ok()
+    .body("Hello world!")
+    .with_status(StatusCode::OK)
+} 
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+  HttpResponse::Ok().body(req_body)
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| App::new().service(hello).service(echo))
+    .bind("127.0.0.1:4949")?
+    .run()
+    .await
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use std::future::Future;
+//     use super::*;
+//     #[actix_rt::test]
+//     async fn test_hello_ok() -> dyn Future<Result<()>, Output = String> {
+//         let resp = hello().await.unwrap();
+//         assert_eq!(resp.with_status(StatusCode::<OK>));
+//         return resp;
+//     }
+// }
